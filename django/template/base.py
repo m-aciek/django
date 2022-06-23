bedab50bcb134f33a96f7e07949bf905de4d2444
Maciej Olko <maciej.olko@gmail.com>
2022-06-23 16:29:35 +0200

    WIP: templates

--- django/template/base.py
+++ django/template/base.py
@@ -62,7 +62,7 @@ from django.utils.regex_helper import _lazy_re_compile
 from django.utils.safestring import SafeData, SafeString, mark_safe
 from django.utils.text import get_text_list, smart_split, unescape_string_literal
 from django.utils.timezone import template_localtime
-from django.utils.translation import gettext_lazy, pgettext_lazy
+from django.utils.translation import gettext_lazy, pgettext_lazy, attributive_translation
 
 from .exceptions import TemplateSyntaxError
 
@@ -1054,6 +1054,8 @@ def render_value_in_context(value, context):
     value = template_localtime(value, use_tz=context.use_tz)
     value = localize(value, use_l10n=context.use_l10n)
     if context.autoescape:
+        # if isinstance(value, attributive_translation.AttributiveTranslationMessage):
+        #     return value
         if not issubclass(type(value), str):
             value = str(value)
         return conditional_escape(value)
